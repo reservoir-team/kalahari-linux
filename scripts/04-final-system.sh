7e2bb0afdc1273dc788a05f8e259d0eb98b65402
Natarizki <natakeren890@gmail.com>
2026-09-07 05:00:56 +0700

    Fix GMP: force generic x86_64 build/host to avoid CPU-specific -march flag (znver3) that fails on GitHub Actions runner CPU

--- scripts/04-final-system.sh
+++ scripts/04-final-system.sh
@@ -171,7 +171,7 @@ cd "$LFS/sources"
 
 echo "==> GMP"
 tar -xf gmp-*.tar.xz && cd gmp-*/
-./configure --prefix=/usr --disable-cxx --disable-static --docdir=/usr/share/doc/gmp
+./configure --prefix=/usr --disable-cxx --disable-static --docdir=/usr/share/doc/gmp --build=x86_64-pc-linux-gnu --host=x86_64-pc-linux-gnu
 make
 make DESTDIR="$LFS" install
 cd "$LFS/sources"
